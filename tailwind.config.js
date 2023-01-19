11353aad12e25355e075b51c977a6088e652b8c6
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2023-01-19 20:37:13 +0000

    hero complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,11 @@ module.exports = {
     container: {
         center: true,
         padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
     },
   },
   plugins: [],
