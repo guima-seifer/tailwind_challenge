fbeeae42502655e4bc30fec4916243debe3ce172
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2023-01-19 19:51:05 +0000

    hero and buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,21 @@
 module.exports = {
   content: ["public/*.{html,js}"],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: 'hsl(231, 69%, 60%)',
+        primaryRed: 'hsl(0, 94%, 66%)',
+        neutralGrayishBlue: 'hsl(229, 8%, 60%)',
+        neutralVeryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+    },
     fontFamily: {
       Rubik: ['Rubik', 'sans-serif']
     }, 
-    colors: {
-      primaryBlue: 'hsl(231, 69%, 60%)',
-      primaryRed: 'hsl(0, 94%, 66%)',
-      neutralGrayishBlue: 'hsl(229, 8%, 60%)',
-      neutralVeryDarkBlue: 'hsl(229, 31%, 21%)',
-    },
+    
     container: {
         center: true,
         padding: "1rem",
-        screens: {
-          "2xl": "1440px",
-          lg: "1440px",
-          xl: "1440px",
-          md: "375px",
-          sm: "375px",
-        },
-      
     },
   },
   plugins: [],
