74f8f983b37abc8a847a3aabf536fca19f2c4b1a
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2023-01-21 22:26:50 +0000

    adapt to mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/*.{html,js}"],
-    theme: {
+  content: ['public/*.{html,js}'],
+  theme: {
     extend: {
       colors: {
         primaryBlue: 'hsl(231, 69%, 60%)',
@@ -11,17 +11,16 @@ module.exports = {
       },
     },
     fontFamily: {
-      Rubik: ['Rubik', 'sans-serif']
-    }, 
-    
+      Rubik: ['Rubik', 'sans-serif'],
+    },
+
     container: {
-        center: true,
-        padding: "1rem",
-        screens: {
-          lg: "1124px",
-          xl: "1124px",
-          "2xl": "1124px",
-        },
+      center: true,
+      padding: '1rem',
+      screens: {
+        xs: '375px',
+        '2xl': '1440px',
+      },
     },
   },
   plugins: [],
