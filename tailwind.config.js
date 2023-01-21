99d0fb77dc63fbc3f868056a0af6e2822f2f749b
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2023-01-21 23:06:33 +0000

    screen tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@
 module.exports = {
   content: ['public/*.{html,js}'],
   theme: {
+    
+
     extend: {
       colors: {
         primaryBlue: 'hsl(231, 69%, 60%)',
         primaryRed: 'hsl(0, 94%, 66%)',
         neutralGrayishBlue: 'hsl(229, 8%, 60%)',
         neutralVeryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontSize: {
+        paragraph: `18px;`
       },
     },
     fontFamily: {
